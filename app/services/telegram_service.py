12f0553c62f639c42e282273fae59b621d02a820
Zhen Zhong <auroradysis@gmail.com>
2023-07-02 00:31:52 +0800

    rename files

---
+++ app/services/telegram_service.py
@@ -0,0 +1,5 @@
+# TODO: Implement Telegram Service
+# example: https://docs.python-telegram-bot.org/en/stable/examples.customwebhookbot.html
+
+from telegram import Bot
+from telegram.ext import Application
