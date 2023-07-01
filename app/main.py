660a2e17e530945747dbcfeb35207fce8daa62f3
Zhen Zhong <auroradysis@gmail.com>
2023-07-02 01:27:58 +0800

    add sentry_sdk

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 from fastapi import FastAPI
 
+import sentry_sdk
+
+SENTRY_DSN = ""
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+)
+
 app = FastAPI()
---
+++ app/models/media_type.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class MediaType(Enum):
+    PHOTO = 1
+    VIDEO = 2
+    ANIMATION = 3
