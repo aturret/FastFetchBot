ef7b5fe988af62b924e1e49a880be8ef7f61c023
aturret <enturreopy@gmail.com>
2023-07-30 23:29:06 -0400

    Remove customized to_dict() method of threads service

--- app/services/threads/__init__.py
+++ app/services/threads/__init__.py
@@ -7,6 +7,7 @@ from playwright.async_api import async_playwright
 
 from app.utils.parse import get_html_text_length, unix_timestamp_to_utc
 from app.models.metadata_item import MetadataItem, MediaFile
+from app.config import HTTP_REQUEST_TIMEOUT
 
 SHORT_LIMIT = 600
 
@@ -29,15 +30,6 @@ class Threads(MetadataItem):
         self.code = urlparse(url).path.split("/")[2]
         self.pics_url = []
         self.videos_url = []
-
-    def to_dict(self):
-        res = {}
-        for k, v in self.__dict__.items():
-            if isinstance(v, Threads):
-                res[k] = v.to_dict()
-            else:
-                res[k] = v
-        return res
 
     async def get_threads(self):
         thread_data = await self.scrape_thread_data(self.url)
@@ -81,7 +73,7 @@ class Threads(MetadataItem):
         """Scrape Threads post and replies from a given URL"""
         _xhr_calls = []
 
-        def intercept_response(response):
+        async def intercept_response(response):
             """capture all background requests and save them"""
             if response.request.resource_type == "xhr":
                 _xhr_calls.append(response)
@@ -89,30 +81,23 @@ class Threads(MetadataItem):
 
         async with async_playwright() as pw:
             browser = await pw.chromium.launch()
-            context = await browser.new_context(
-                viewport={"width": 1920, "height": 1080}
-            )
+            context = await browser.new_context(viewport={"width": 1920, "height": 1080})
             page = await context.new_page()
-            page.on(
-                "response", intercept_response
-            )  # enable background request intercepting
+            page.on("response", intercept_response)  # enable background request intercepting
             await page.goto(url)  # go to url and wait for the page to load
-            await page.wait_for_selector(
-                "[data-pressable-container=true]"
-            )  # wait for page to finish loading
+            await page.wait_for_selector("[data-pressable-container=true]")  # wait for page to finish loading
             # find all thread related background requests:
             gql_calls = [f for f in _xhr_calls if "/api/graphql" in f.url]
             thread_data = {
                 "threads": [],
             }
             for xhr in gql_calls:
-                data = json.loads(xhr.text())
+                text = await xhr.text()
+                data = json.loads(text)
                 print(json.dumps(data, indent=4, ensure_ascii=False))
                 threads = data["data"]["data"]["containing_thread"]["thread_items"]
                 for thread in threads:
-                    thread_data["threads"].append(
-                        self.parse_single_threads_data(thread["post"])
-                    )
+                    thread_data["threads"].append(self.parse_single_threads_data(thread["post"]))
             return thread_data
 
     def process_threads_item(self, thread_data: Dict) -> None:
@@ -151,24 +136,16 @@ class Threads(MetadataItem):
         }
         # make html components, and solve the pictures and videos
         user_component = f"<a href='https://threads.net/@{thread['username']}'>@{thread['username']}</a>:"
-        thread_info["content_group"] += user_component + thread["text"].replace(
-            "\n", "<br><br>"
-        )
+        thread_info["content_group"] += user_component + thread["text"].replace("\n", "<br><br>")
         thread_info["text_group"] += user_component + thread["text"] + "\n"
         if not thread["media_count"]:  # if the thread doesn't have multiple media files
             if thread["video"]:  # if the threads has only one video/gif
                 thread_info["videos_url"].append(thread["video"])
-                thread_info["media_files"].append(
-                    MediaFile(media_type="video", url=thread["video"], caption="")
-                )
-                thread_info[
-                    "content_group"
-                ] += f"<video controls=\"controls\" src=\"{thread['video']}\">"
+                thread_info["media_files"].append(MediaFile(media_type="video", url=thread["video"], caption=""))
+                thread_info["content_group"] += f"<video controls=\"controls\" src=\"{thread['video']}\">"
             elif thread["image"]:  # if the threads has only one picture
                 thread_info["pics_url"].append(thread["image"])
-                thread_info["media_files"].append(
-                    MediaFile(media_type="image", url=thread["image"], caption="")
-                )
+                thread_info["media_files"].append(MediaFile(media_type="image", url=thread["image"], caption=""))
                 thread_info["content_group"] += f"<img src=\"{thread['image']}\">"
         else:  # if the threads has more than one media files
             for media in thread["media_files"]:
@@ -185,9 +162,7 @@ class Threads(MetadataItem):
                         "content_group"
                     ] += f"<video controls=\"controls\" src=\"{media['video_versions'][0]['url']}\">"
                 else:  # if the media is a picture
-                    thread_info["pics_url"].append(
-                        media["image_versions2"]["candidates"][0]["url"]
-                    )
+                    thread_info["pics_url"].append(media["image_versions2"]["candidates"][0]["url"])
                     thread_info["media_files"].append(
                         MediaFile(
                             media_type="image",
@@ -195,21 +170,13 @@ class Threads(MetadataItem):
                             caption="",
                         )
                     )
-                    thread_info[
-                        "content_group"
-                    ] += f"<img src=\"{media['image_versions2']['candidates'][0]['url']}\">"
+                    thread_info["content_group"] += f"<img src=\"{media['image_versions2']['candidates'][0]['url']}\">"
         thread_info["content_group"] += "<hr>"
         if thread["link"]:  # process the link item in the threads
             link_title = thread["link"]["title"]
-            link_url = (
-                unquote(urlparse(thread["link"]["url"]).query)
-                .split("=")[1]
-                .split("&")[0]
-            )
+            link_url = unquote(urlparse(thread["link"]["url"]).query).split("=")[1].split("&")[0]
             thread_info["text_group"] += f"<a href='{link_url}'>{link_title}</a>\n"
-            thread_info[
-                "content_group"
-            ] += f"<p><a href='{link_url}'>{link_title}</a></p><br>"
+            thread_info["content_group"] += f"<p><a href='{link_url}'>{link_title}</a></p><br>"
         if thread["quoted_post"] is not None:  # solve possible retweeted threads
             retweeted_thread = Threads.parse_single_threads_data(thread["quoted_post"])
             retweeted_thread_info = Threads.parse_single_threads(retweeted_thread)
