551ca0a4a46faa98d17602c635c987ad9f9a2cbf
aturret <enturreopy@gmail.com>
2023-08-02 23:55:10 -0400

    Add upload_image to telegraph

--- app/services/telegraph/__init__.py
+++ app/services/telegraph/__init__.py
@@ -1,13 +1,24 @@
 # TODO: copy the html-to-telegraph package and modify it to fit the asynchronous model
 from typing import Any
 
-from app.services.html_telegraph_poster_async import upload_to_telegraph, AsyncTelegraphPoster
+from app.services.html_telegraph_poster_async import (
+    upload_to_telegraph,
+    AsyncTelegraphPoster,
+)
+from app.services.html_telegraph_poster_async.utils import DocumentPreprocessor
 from app.models.telegraph_item import TelegraphItem, from_str
 
 
 class Telegraph(TelegraphItem):
-
-    def __init__(self, title: str, url: str, author: str, author_url: str, category: str, content: str):
+    def __init__(
+        self,
+        title: str,
+        url: str,
+        author: str,
+        author_url: str,
+        category: str,
+        content: str,
+    ):
         self.telegraph = AsyncTelegraphPoster(use_api=True)
         self.title = title
         self.url = url
@@ -17,7 +28,7 @@ class Telegraph(TelegraphItem):
         self.content = content
 
     @staticmethod
-    def from_dict(obj: Any) -> 'Telegraph':
+    def from_dict(obj: Any) -> "Telegraph":
         assert isinstance(obj, dict)
         title = from_str(obj.get("title"))
         url = from_str(obj.get("url"))
@@ -27,11 +38,19 @@ class Telegraph(TelegraphItem):
         content = from_str(obj.get("content"))
         return Telegraph(title, url, author, author_url, category, content)
 
-    async def get_telegraph(self):
-        await self.telegraph.create_api_token(short_name=self.author[0:14], author_name=self.author)
-        telegraph_post = await self.telegraph.post(title=self.title,
-                                                   author=self.author,
-                                                   author_url=self.author_url,
-                                                   text=self.content)
-        telegraph_url = telegraph_post['url']
+    async def get_telegraph(self, upload_images: bool = True) -> str:
+        if upload_images:
+            temp_html = DocumentPreprocessor(self.content)
+            await temp_html.upload_all_images()
+            self.content = temp_html.get_processed_html()
+        await self.telegraph.create_api_token(
+            short_name=self.author[0:14], author_name=self.author
+        )
+        telegraph_post = await self.telegraph.post(
+            title=self.title,
+            author=self.author,
+            author_url=self.author_url,
+            text=self.content,
+        )
+        telegraph_url = telegraph_post["url"]
         return telegraph_url
