08137c3514f2efe98caf8c0de7762075418e6013
aturret <enturreopy@gmail.com>
2023-08-07 20:36:11 -0400

    Update weibo pic and text processing

--- app/services/weibo/__init__.py
+++ app/services/weibo/__init__.py
@@ -4,21 +4,35 @@ from urllib.parse import urlparse
 
 import httpx
 import jmespath
+from bs4 import BeautifulSoup
+from lxml import html
 
 from app.models.metadata_item import MetadataItem, MediaFile
 from app.utils.config import CHROME_USER_AGENT
 from app.utils.network import get_response_json
-from .config import (AJAX_HOST, AJAX_LONGTEXT_HOST, WEIBO_WEB_HOST, WEIBO_HOST)
+from app.utils.parse import get_html_text_length
+from .config import (
+    AJAX_HOST,
+    AJAX_LONGTEXT_HOST,
+    WEIBO_WEB_HOST,
+    WEIBO_HOST,
+    WEIBO_TEXT_LIMIT,
+)
+from app.config import JINJA2_ENV
+
+environment = JINJA2_ENV
+short_text_template = environment.get_template("short_text.html")
+content_template = environment.get_template("weibo_content.html")
 
 
 class Weibo(MetadataItem):
     def __init__(
-            self,
-            url: str,
-            method: Optional[str] = "api",
-            scraper: Optional[str] = "requests",
-            user_agent: Optional[dict] = CHROME_USER_AGENT,
-            cookies: Optional[str] = None,
+        self,
+        url: str,
+        method: Optional[str] = "api",
+        scraper: Optional[str] = "requests",
+        user_agent: Optional[dict] = CHROME_USER_AGENT,
+        cookies: Optional[str] = None,
     ):
         # basic info
         self.url = url
@@ -33,6 +47,8 @@ class Weibo(MetadataItem):
         self.media_files = []
         self.author = ""
         self.author_url = ""
+        # auxiliary fields
+        self.retweeted_info = {}
 
     async def get_weibo(self):
         weibo_info = await self._get_weibo_info()
@@ -57,7 +73,7 @@ class Weibo(MetadataItem):
                 new_url = response.headers["Location"]
                 response = await client.get(new_url, headers=self.headers)
         html = response.text
-        html = html[html.find('"status":'):]
+        html = html[html.find('"status":') :]
         html = html[: html.rfind('"hotScheme"')]
         html = html[: html.rfind(",")]
         html = html[: html.rfind("][0] || {};")]
@@ -78,41 +94,56 @@ class Weibo(MetadataItem):
         return ajax_json
 
     async def _process_weibo_item(self, weibo_info: dict) -> None:
-        self.id = weibo_info["id"]
+        self.id = weibo_info.get("id")
         # get user info
-        self.user_id = weibo_info["user_id"]
-        self.author = weibo_info["author"]
-        self.author_url = WEIBO_HOST + weibo_info["author_url"]
+        self.user_id = weibo_info.get("user_id")
+        self.author = weibo_info.get("author")
+        self.author_url = WEIBO_HOST + weibo_info.get("author_url")
         self.title = self.author + "的微博"
         # get basic metadata
-        self.date = weibo_info["created_at"]
-        self.source = weibo_info["source"] if "source" in weibo_info else None
-        self.region_name = weibo_info['region_name'] if 'region_name' in weibo_info else None
+        self.date = weibo_info.get("created")
+        self.source = weibo_info.get("source", None)
+        self.region_name = weibo_info.get("region_name", None)
         self.attitudes_count = self._string_to_int(weibo_info.get("attitudes_count", 0))
         self.comments_count = self._string_to_int(weibo_info.get("comments_count", 0))
         self.reposts_count = self._string_to_int(weibo_info.get("reposts_count", 0))
         # resolve text
         # check if the weibo is longtext weibo (which means >140 characters so has an excerpt) or not
-        if not weibo_info['is_long_text'] or (weibo_info['pic_num'] > 9 and weibo_info['is_long_text']):
+        if not weibo_info["is_long_text"] or (
+            weibo_info["pic_num"] > 9 and weibo_info["is_long_text"]
+        ):
             # if a weibo has more than 9 pictures, the isLongText will be True even if it is not a longtext weibo
             # however, we cannot get the full text of such kind of weibo from longtext api (it will return None)
             # so, it is necessary to check if a weibo is a real longtext weibo or not for getting the full text
-            self.type = "short"
-
-        # resolve pictures
-        pic_list = self._get_pictures(weibo_info)
-
-        # resolve videos
-        video_list = self._get_videos(weibo_info)
-        live_photo_list = self._get_live_photo(weibo_info)
-        if live_photo_list:
-            video_list += live_photo_list
-        for video_url in video_list:
-            self.media_files.append(MediaFile(url=video_url, media_type="video"))
-
+            text = weibo_info.get("text")
+            # TODO: add a branch to get the fulltext without using the webpage scraping. This branch needs cookies.
+        else:
+            longtext_info = await self._get_weibo_info(method="webpage")
+            text = longtext_info.get("text")
+        cleaned_text, fw_pics = self._weibo_html_text_clean(text)
+        for pic in fw_pics:
+            self.media_files.append(MediaFile(url=pic, media_type="image"))
+        self.text += cleaned_text.replace("<br />", "<br>").replace("br/", "br")
+        self.raw_content = self.text
+        # resolve medias
+        self.media_files = self._get_media_files(weibo_info)
+        # render the text and content
+        data = self.__dict__
+        self.text = short_text_template.render(data=data)
+        self.content = content_template.render(data=data)
         # resolve retweet
         if weibo_info.get("retweeted_status"):
-            retweet_info = self._parse_weibo_info(weibo_info["retweeted_status"])
+            retweeted_weibo_item = Weibo(
+                url=WEIBO_HOST + weibo_info["retweeted_status"]["url"]
+            )
+            self.retweeted_info = await retweeted_weibo_item.get_weibo()
+            self.text += self.retweeted_info["text"]
+            self.content += "<hr>" + self.retweeted_info["content"]
+            self.media_files += self.retweeted_info["media_files"]
+        # type check
+        self.type = (
+            "long" if get_html_text_length(self.text) > WEIBO_TEXT_LIMIT else "short"
+        )
 
     @staticmethod
     def _parse_weibo_info(data: dict) -> dict:
@@ -142,60 +173,149 @@ class Weibo(MetadataItem):
         weibo_info = jmespath.search(expression, data)
         return weibo_info
 
+    def _get_media_files(self, weibo_info: dict) -> list:
+        """
+        The function is used to get all media files (pictures, videos, live photos) from a weibo item
+        The design of weibo media files is very complicated and confusing. It can be divided from the following aspects:
+        1. pic_infos: the media files of a weibo item are stored in pic_infos. This key only appears when the weibo item
+        has only pictures. However, live photos, which is actually "videos", are also stored in pic_infos. So, we need
+        to check the type and add it into the media files list.
+        2. page_info: the media files of a weibo item are stored in page_info. This key only appears when the weibo item
+        has only one video.
+        3. mix_media_info: the media files of a weibo item are stored in mix_media_info. This key only appears when the
+        weibo item has both pictures and videos.
+        We separate the media files scraping process into three parts according to the above aspects. For keeping the
+        order of the media files, we use a list to store the media files.
+        :param weibo_info:
+        :return: media_files: a list of media files
+        """
+        media_files = []
+        media_files += self._get_pictures(weibo_info)
+        media_files += self._get_videos(weibo_info)
+        media_files += self._get_mix_media(weibo_info)
+        return media_files
+
     @staticmethod
     def _get_pictures(weibo_info: dict) -> list:
-        pic_list = []
-        if weibo_info.get('pic_num'):
-            for i in range(weibo_info['pic_num']):
-                pic_url = weibo_info['pic_infos'][i]['large']['url']
-                pic_list.append(pic_url)
-        return pic_list
+        media_files = []
+        if weibo_info.get("pics"):
+            pic_info = weibo_info["pics"]
+            for pic in pic_info:
+                media_files.append(
+                    MediaFile(url=pic["large"]["url"], media_type="image")
+                )
+        elif "pic_infos" in weibo_info and weibo_info.get("pic_num") > 0:
+            pic_info = weibo_info["pic_infos"]
+            for pic in pic_info:
+                if pic_info[pic].get("type") == "pic":
+                    media_files.append(
+                        MediaFile(
+                            url=pic_info[pic]["original"]["url"], media_type="image"
+                        )
+                    ) if pic_info[pic]["original"] else media_files.append(
+                        MediaFile(url=pic_info[pic]["large"]["url"], media_type="image")
+                    )
+                elif pic_info[pic].get("type") in ["live_photo", "livephoto"]:
+                    media_files.append(
+                        MediaFile(
+                            url=pic_info[pic]["original"]["url"], media_type="image"
+                        )
+                    ) if pic_info[pic]["original"] else media_files.append(
+                        MediaFile(pic_info[pic]["large"]["url"])
+                    )
+                    media_files.append(
+                        MediaFile(url=pic_info[pic]["video"], media_type="video")
+                    )
+                elif pic_info[pic].get("type") == "gif":
+                    media_files.append(
+                        MediaFile(url=pic_info[pic]["video"], media_type="video")
+                    )
+        else:
+            return media_files
+        return media_files
 
     @staticmethod
     def _get_videos(weibo_info: dict) -> list:
-        video_url_list = []
-        if weibo_info.get('page_info'):
-            if ((weibo_info['page_info'].get('urls')
-                 or weibo_info['page_info'].get('media_info'))
-                    and (weibo_info['page_info'].get('type') == 'video'
-                         or weibo_info['page_info'].get('object_type') == 'video')):
-                media_info = weibo_info['page_info']['urls'] if weibo_info['page_info'].get('urls') else ''
+        media_files, video_url_list = [], []
+        if weibo_info.get("page_info"):
+            if (
+                weibo_info["page_info"].get("urls")
+                or weibo_info["page_info"].get("media_info")
+            ) and (
+                weibo_info["page_info"].get("type") == "video"
+                or weibo_info["page_info"].get("object_type") == "video"
+            ):
+                media_info = (
+                    weibo_info["page_info"]["urls"]
+                    if weibo_info["page_info"].get("urls")
+                    else ""
+                )
                 if not media_info:
-                    media_info = weibo_info['page_info']['media_info']
-                video_url = media_info.get('mp4_720p_mp4')
+                    media_info = weibo_info["page_info"]["media_info"]
+                video_url = media_info.get("mp4_720p_mp4")
                 if not video_url:
-                    video_url = media_info.get('mp4_hd_url')
+                    video_url = media_info.get("mp4_hd_url")
                 if not video_url:
-                    video_url = media_info.get('hevc_mp4_hd')
+                    video_url = media_info.get("hevc_mp4_hd")
                 if not video_url:
-                    video_url = media_info.get('mp4_sd_url')
+                    video_url = media_info.get("mp4_sd_url")
                 if not video_url:
-                    video_url = media_info.get('mp4_ld_mp4')
+                    video_url = media_info.get("mp4_ld_mp4")
                 if not video_url:
-                    video_url = media_info.get('stream_url_hd')
+                    video_url = media_info.get("stream_url_hd")
                 if not video_url:
-                    video_url = media_info.get('stream_url')
+                    video_url = media_info.get("stream_url")
                 video_url_list.append(video_url)
-        if weibo_info.get('mix_media_info'):
-            for items in weibo_info['mix_media_info']['items']:
-                if items.get('type') == 'video':
-                    video_url = items.get('stream_url_hd')
+        for url in video_url_list:
+            media_files.append(MediaFile(url=url, media_type="video"))
+        return media_files
+
+    @staticmethod
+    def _get_mix_media(weibo_info: dict) -> list:
+        media_files = []
+        if weibo_info.get("mix_media_info"):
+            for item in weibo_info["mix_media_info"]["items"]:
+                if item.get("type") == "pic":
+                    media_files.append(
+                        MediaFile(
+                            url=item["data"]["original"]["url"], media_type="image"
+                        )
+                    ) if item["data"]["original"] else media_files.append(
+                        MediaFile(url=item["data"]["large"]["url"], media_type="image")
+                    )
+                elif item.get("type") in ["live_photo", "livephoto"]:
+                    media_files.append(
+                        MediaFile(
+                            url=item["data"]["original"]["url"], media_type="image"
+                        )
+                    ) if item["data"]["original"] else media_files.append(
+                        MediaFile(url=item["data"]["large"]["url"], media_type="image")
+                    )
+                    media_files.append(
+                        MediaFile(url=item["data"]["video"]["url"], media_type="video")
+                    )
+                elif item.get("type") == "gif":
+                    media_files.append(
+                        MediaFile(url=item["data"]["video"]["url"], media_type="video")
+                    )
+                elif item.get("type") == "video":
+                    video_url = item.get("stream_url_hd")
                     if not video_url:
-                        video_url = items['data']['media_info'].get('mp4_720p_mp4')
+                        video_url = item["data"]["media_info"].get("mp4_720p_mp4")
                     if not video_url:
-                        video_url = items['data']['media_info'].get('mp4_hd_url')
+                        video_url = item["data"]["media_info"].get("mp4_hd_url")
                     if not video_url:
-                        video_url = items['data']['media_info'].get('hevc_mp4_hd')
+                        video_url = item["data"]["media_info"].get("hevc_mp4_hd")
                     if not video_url:
-                        video_url = items['data']['media_info'].get('mp4_sd_url')
+                        video_url = item["data"]["media_info"].get("mp4_sd_url")
                     if not video_url:
-                        video_url = items['data']['media_info'].get('mp4_ld_mp4')
+                        video_url = item["data"]["media_info"].get("mp4_ld_mp4")
                     if not video_url:
-                        video_url = items['data']['media_info'].get('stream_url_hd')
+                        video_url = item["data"]["media_info"].get("stream_url_hd")
                     if not video_url:
-                        video_url = items['data']['media_info'].get('stream_url')
-                    video_url_list.append(video_url)
-        return video_url_list
+                        video_url = item["data"]["media_info"].get("stream_url")
+                    media_files.append(MediaFile(url=video_url, media_type="video"))
+        return media_files
 
     @staticmethod
     def _string_to_int(string: str) -> int:
@@ -206,22 +326,60 @@ class Weibo(MetadataItem):
         """
         if isinstance(string, int):
             return string
-        elif string.endswith(u'万+'):
-            string = string[:-2] + '0000'
-        elif string.endswith(u'万'):
+        elif string.endswith("万+"):
+            string = string[:-2] + "0000"
+        elif string.endswith("万"):
             string = float(string[:-1]) * 10000
-        elif string.endswith(u'亿'):
+        elif string.endswith("亿"):
             string = float(string[:-1]) * 100000000
         return int(string)
 
     @staticmethod
     def _get_live_photo(weibo_info: dict) -> list:
         live_photo_list = []
-        live_photo = weibo_info.get('pic_video')
+        live_photo = weibo_info.get("pic_video")
         if live_photo:
-            prefix = 'https://video.weibo.com/media/play?livephoto=//us.sinaimg.cn/'
-            for i in live_photo.split(','):
-                if len(i.split(':')) == 2:
-                    url = prefix + i.split(':')[1] + '.mov'
+            prefix = "https://video.weibo.com/media/play?livephoto=//us.sinaimg.cn/"
+            for i in live_photo.split(","):
+                if len(i.split(":")) == 2:
+                    url = prefix + i.split(":")[1] + ".mov"
                     live_photo_list.append(url)
             return live_photo_list
+
+    @staticmethod
+    def _weibo_html_text_clean(text, method="bs4"):
+        fw_pics = []
+        if method == "bs4":
+            soup = BeautifulSoup(text, "html.parser")
+            for img in soup.find_all("img"):
+                alt_text = img.get("alt", "")
+                img.replace_with(alt_text)
+            for a in soup.find_all("a"):
+                if a.text == "查看图片":
+                    fw_pics.append(a.get("href"))
+            if "/n/" in a.get("href") and a.get("usercard"):
+                a["href"] = "https://weibo.com" + a.get("href")
+            for i in soup.find_all("span"):
+                i.unwrap()
+            res = (
+                str(soup)
+                .replace('href="//', 'href="http://')
+                .replace('href="/n/', 'href="http://weibo.com/n/')
+            )
+            return res, fw_pics
+
+        if method == "lxml":
+            selector = html.fromstring(text)
+            # remove all img tags and replace with alt text
+            for img in selector.xpath("//img"):
+                alt_text = img.get("alt", "")
+            # get innerhtml pure text of the parent tag
+            parent_text = img.getparent().text_content() if img.getparent() else ""
+            replace_text = alt_text + parent_text
+            text_node = html.fromstring(replace_text)
+            img.addprevious(text_node)
+            img.getparent().remove(img)
+            # make text_node become pure text
+            text_node.text = text_node.text_content()
+            # return the html document after cleaning
+            return html.tostring(selector, encoding="unicode")
