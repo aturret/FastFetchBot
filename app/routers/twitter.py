d34e6ebfbf49eb492dcd2f6260b2ac2a39c2457f
Zhen Zhong <auroradysis@gmail.com>
2023-08-24 17:56:03 +0100

    Create twitter api

---
+++ app/routers/twitter.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+
+from app.services.common import InfoExtractService
+from fastapi import Security
+from app.auth import verify_api_key
+
+
+router = APIRouter(prefix="/twitter")
+
+
+@router.post("/repost", dependencies=[Security(verify_api_key)])
+async def twitter_repost_webhook(url: str):
+    url_metadata = {
+        "url": url,
+        "type": "social_media",
+        "source": "twitter",
+    }
+    item = InfoExtractService(url_metadata)
+    await item.get_item()
+    return "ok"
