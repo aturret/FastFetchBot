8ca8b394374004d46073953ae49dfa42bc2eb516
aturret <enturreopy@gmail.com>
2023-08-26 16:38:32 -0400

    Add test cases for zhihu and douban

---
+++ tests/cases/douban.py
@@ -0,0 +1,7 @@
+douban_cases = {
+    "long_status": "http://www.douban.com/people/jenniferpsy/status/4326599964/",
+    "long_note": "https://www.douban.com/note/841420545/",
+    "movie_review": "https://movie.douban.com/review/15328612/",
+    "group_article": "https://www.douban.com/group/topic/291851553/",
+    "book_review": "https://m.douban.com/book/review/1541532/",
+}
