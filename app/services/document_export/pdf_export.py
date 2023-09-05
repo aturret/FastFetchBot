999c34dbe7ff9ac5d63e2703c1620e3455842d64
aturret <enturreopy@gmail.com>
2023-09-05 13:51:34 -0400

    Reconstructure pdf export function

--- app/services/document_export/pdf_export.py
+++ app/services/document_export/pdf_export.py
@@ -1,15 +1,19 @@
 import asyncio
 import functools
-import gc
+
+# import gc
 import os
 import uuid
 
-# from xhtml2pdf import pisa
-from weasyprint import HTML, CSS
-from bs4 import BeautifulSoup
-from weasyprint.text.fonts import FontConfiguration
+import httpx
 
-from app.config import DOWNLOAD_DIR
+# from xhtml2pdf import pisa
+# from weasyprint import HTML, CSS
+from bs4 import BeautifulSoup
+
+# from weasyprint.text.fonts import FontConfiguration
+
+from app.config import DOWNLOAD_DIR, FILE_EXPORTER_URL
 from app.utils.logger import logger
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -24,7 +28,7 @@ class PdfExport:
 
     async def export(self) -> str:
         html_string = self.wrap_html_string(self.html_string)
-        output_filename = os.path.join(DOWNLOAD_DIR, f"{self.title}-{uuid.uuid4()}.pdf")
+        output_filename = f"{self.title}-{uuid.uuid4()}.pdf"
         loop = asyncio.get_event_loop()
         css_string = await loop.run_in_executor(None, open, PDF_STYLESHEET, "r")
         css_string = await loop.run_in_executor(None, css_string.read)
@@ -33,13 +37,17 @@ class PdfExport:
         html_string: {html_string}
         """
         )
-        await self.convert_html_to_pdf(
-            html_string=html_string,
-            css_string=css_string,
-            output_filename=output_filename,
-        )
+        async with httpx.AsyncClient() as client:
+            body = {
+                "html_string": html_string,
+                "css_string": css_string,
+                "output_filename": output_filename,
+            }
+            request_url = FILE_EXPORTER_URL + "/pdfExport"
+            logger.info(f"requesting pdf export from pdf server: {body}")
+            resp = await client.post(request_url, json=body)
+        output_filename = resp.json().get("output_filename")
         return output_filename
-        pass
 
     @staticmethod
     def wrap_html_string(html_string: str) -> str:
@@ -56,22 +64,22 @@ class PdfExport:
             style_tag.decompose()
         return soup.prettify()
 
-    @staticmethod
-    async def convert_html_to_pdf(
-        html_string: str, css_string: str, output_filename: str
-    ) -> None:
-        font_config = FontConfiguration()
-        css_item = CSS(string=css_string, font_config=font_config)
-        html_item = HTML(string=html_string)
-        loop = asyncio.get_event_loop()
-        pdf_obj = await loop.run_in_executor(
-            None,
-            functools.partial(
-                html_item.write_pdf, output_filename, stylesheets=[css_item]
-            ),
-        )
-        del font_config
-        del css_item
-        del html_item
-        del pdf_obj
-        gc.collect()
+    # @staticmethod
+    # async def convert_html_to_pdf(
+    #     html_string: str, css_string: str, output_filename: str
+    # ) -> None:
+    #     font_config = FontConfiguration()
+    #     css_item = CSS(string=css_string, font_config=font_config)
+    #     html_item = HTML(string=html_string)
+    #     loop = asyncio.get_event_loop()
+    #     pdf_obj = await loop.run_in_executor(
+    #         None,
+    #         functools.partial(
+    #             html_item.write_pdf, output_filename, stylesheets=[css_item]
+    #         ),
+    #     )
+    #     del font_config
+    #     del css_item
+    #     del html_item
+    #     del pdf_obj
+    #     gc.collect()
