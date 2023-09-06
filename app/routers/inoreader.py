e49c9977265da1e9cb8d76ffaee02159d47225bb
aturret <enturreopy@gmail.com>
2023-09-05 22:17:40 -0400

    Add auto transfer video for Inoreader

--- app/routers/inoreader.py
+++ app/routers/inoreader.py
@@ -10,6 +10,7 @@ from app.services.common import InfoExtractService
 from app.services.inoreader import Inoreader
 from fastapi import Security
 from app.auth import verify_api_key
+from app.utils.parse import check_url_type
 
 router = APIRouter(prefix="/inoreader")
 telegram_channel_id = TELEGRAM_CHANNEL_ID[0] if TELEGRAM_CHANNEL_ID else None
@@ -35,12 +36,19 @@ def get_inoreader_item(data: dict, trigger: bool = False):
 async def get_inoreader_item_async(data: dict, trigger: bool = False):
     if trigger:
         data = await Inoreader.request_api_info()
+    url_type_item = await check_url_type(data["aurl"])
+    url_type_dict = url_type_item.to_dict()
+    is_video = url_type_dict["content_type"] == "video"
+    content_type = url_type_dict["content_type"] if is_video else "social_media"
+    source = url_type_dict["source"] if is_video else "inoreader"
+
     url_metadata = UrlMetadata(
         url=data["aurl"],
-        content_type="social_media",
-        source="inoreader",
+        content_type=content_type,
+        source=source,
     )
     item = InfoExtractService(url_metadata=url_metadata, data=data, store_document=True)
+
     metadata_item = await item.get_item()
     await send_item_message(metadata_item, chat_id=telegram_channel_id)
 
@@ -54,7 +62,7 @@ async def inoreader_repost_webhook(request: Request, background_tasks: Backgroun
 
 @router.post("/trigger", dependencies=[Security(verify_api_key)])
 async def inoreader_trigger_webhook(
-        request: Request, background_tasks: BackgroundTasks
+    request: Request, background_tasks: BackgroundTasks
 ):
     if not INOREADER_APP_ID or not INOREADER_APP_KEY:
         return "inoreader app id or key not set"
@@ -63,11 +71,8 @@ async def inoreader_trigger_webhook(
 
 
 @router.post("/triggerAsync", dependencies=[Security(verify_api_key)])
-async def inoreader_trigger_webhook(
-        request: Request
-):
+async def inoreader_trigger_webhook(request: Request):
     if not INOREADER_APP_ID or not INOREADER_APP_KEY:
         return "inoreader app id or key not set"
     await get_inoreader_item_async({}, True)
     return "ok"
-
