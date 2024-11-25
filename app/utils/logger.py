5cdbdf819ca0af1985e54b9f7afd790cde94ea48
aturret <enturreopy@gmail.com>
2024-11-24 19:44:08 -0500

    Update logger

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,8 +1,18 @@
 import logging
 import os
 
-import loguru
+from loguru import logger
 
 from app.config import LOG_LEVEL, LOG_FILE_PATH
 
-logger = loguru.logger
+log_path = os.path.join(LOG_FILE_PATH, "app.log")
+
+logger.add(
+    log_path,
+    level=LOG_LEVEL,
+    rotation="1 week",
+    retention="10 days",
+    compression="zip",
+)
+logger.debug(f"Logger initialized with level: {LOG_LEVEL}")
+logger.debug(f"Logger initialized with log file path: {log_path}")
