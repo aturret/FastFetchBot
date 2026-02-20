bb17ffc76506cd6dcaab7955a9fac3fb7e8a8e94
aturret <enturreopy@gmail.com>
2026-02-19 23:25:20 -0600

    chore: set unused export function as async

--- apps/api/src/services/file_export/document_export/__init__.py
+++ apps/api/src/services/file_export/document_export/__init__.py
@@ -8,7 +8,7 @@ class DocumentExport(object):
     def __init__(self, document):
         self.document = document
 
-    def export(self):
+    async def export(self):
         if self.document["type"] == "pdf":
             return pdf_export.PdfExport(self.document["content"]).export()
         return None
