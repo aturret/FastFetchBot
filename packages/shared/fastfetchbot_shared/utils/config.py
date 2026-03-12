a7523dc943eff5d8085a893a537f2d0e741e9b26
aturret <enturreopy@gmail.com>
2026-03-11 19:32:13 -0500

    chore: update banned list

--- packages/shared/fastfetchbot_shared/utils/config.py
+++ packages/shared/fastfetchbot_shared/utils/config.py
@@ -50,9 +50,10 @@ VIDEO_WEBSITE_PATTERNS = {
 }
 BANNED_PATTERNS = [
     r"chatgpt\.com\/share\/[A-Za-z0-9]+",
-    r"gemini\/share\/[A-Za-z0-9]+",
+    r"gemini\.google\.com\/share\/[A-Za-z0-9]+",
     r"t\.me\/[A-Za-z0-9]+",
     r"github\.com\/[A-Za-z0-9_-]+\/[A-Za-z0-9_-]+",
-    r"discord\.gg",
+    r"discord\.gg\/[A-Za-z0-9]+",
     r"linkin\.com\/in\/[A-Za-z0-9]+",
+    r"telegra\.ph"
 ]
