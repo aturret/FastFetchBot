5f6cf518cf0be38173987f0a5159e67cfd9572e6
aturret <enturreopy@gmail.com>
2026-07-26 12:03:07 -0500

    Update config.py

--- packages/shared/fastfetchbot_shared/utils/config.py
+++ packages/shared/fastfetchbot_shared/utils/config.py
@@ -28,7 +28,8 @@ SOCIAL_MEDIA_WEBSITE_PATTERNS = {
     "xiaohongshu": [
         r"(www\.)?xiaohongshu\.com\/(discovery\/item|explore)\/[0-9a-zA-Z_-]+",
         r"(www\.)?xhslink\.com\/[0-9a-zA-Z_-]+",
-        r"(www\.)?rednote\.com\/(discovery\/item|explore)\/[0-9a-zA-Z_-]+"
+        r"(www\.)?xhslink\.cn\/[0-9a-zA-Z_-]+",
+        r"(www\.)?rednote\.com\/(discovery\/item|explore)\/[0-9a-zA-Z_-]+",
     ],
     "reddit": [
         r"(www\.)?reddit\.com\/r\/[a-zA-Z0-9_-]+\/comments\/[a-zA-Z0-9_-]+",
